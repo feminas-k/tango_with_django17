ad1dca166e1cb8e2695a9fed27b1b14dcb88c9c8
feminas-k <femi1991@gmail.com>
2016-10-11 20:14:36 +0530

    first commit

--- tango_with_django_project/tango_with_django_project/text.py
+++ tango_with_django_project/tango_with_django_project/text.py
@@ -1,8 +1,8 @@
 import os
-#print __file__
-#print os.path.join(os.path.dirname(__file__), '..')
-#print os.path.dirname(os.path.realpath(__file__))
-#print os.path.abspath(os.path.dirname(__file__))
-#print os.path.dirname(__file__)
+print __file__
+print os.path.join(os.path.dirname(__file__), '..')
+print os.path.dirname(os.path.realpath(__file__))
+print os.path.abspath(os.path.dirname(__file__))
+print os.path.dirname(__file__)
 print os.path.dirname(os.path.dirname(__file__))
 
