bf84ee9f9540ea8540bef0dbb64f4c3c810475b0
feminas-k <femi1991@gmail.com>
2016-10-11 19:17:41 +0530

    first commit

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	context_dict = {'boldmessage': "I am bold font from the context"}
+	return render(request,'rango/index.html', context_dict)
+
+def about(request):
+	context_dict = {'boldmessage': "I am the new bold font"}
+	return render(request,'rango/about.html',context_dict)
+
